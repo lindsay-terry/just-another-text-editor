7c99caeb2ec85381f7006a5a44274bfa46c894cc
Lindsay Terry <lindsaytee66@gmail.com>
2024-08-21 00:05:33 -0600

    Adds configuration to webpack.config for css and babel loaders and service worker and manifest file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -18,12 +15,46 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      // Workbox plugins for a service worker and manifest file.
+      new InjectManifest({
+        swSrc: '.src-sw.js',
+        swDest: 'service-worker.js',
+      }),
+      new WebpackPwaManifest({
+        filename: 'manifest.json',
+        inject: false,
+        fingerprints: false,
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E',
+        description: 'Just another text editor',
+        background_color: '#ffffff',
+        theme_color: '#2196f3',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512]
+          }
+        ]
+      })
     ],
 
     module: {
       rules: [
-        
+        {
+          // CSS loaders and babel to webpack.
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        },
       ],
     },
   };
